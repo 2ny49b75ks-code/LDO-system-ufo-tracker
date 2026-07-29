6957622822dcb65684621e776f84c02ae843ef8b
JD <j_david@live.ca>
2026-07-29 18:33:20 -0400

    Initial Commit

---
+++ LDO system ufo tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LDO system ufo tracker
+//
+//  Created by Jean-David Giroux on 2026-07-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LDO system ufo tracker/LDO_system_ufo_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LDO_system_ufo_trackerApp.swift
+//  LDO system ufo tracker
+//
+//  Created by Jean-David Giroux on 2026-07-29.
+//
+
+import SwiftUI
+
+@main
+struct LDO_system_ufo_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
